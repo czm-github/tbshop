f0415626d14a94e877d318625d6887c586f69977
czmgithub9373 <czm-github>
2020-02-07 16:00:54 +0800

    添加仿taobao商城项目

---
+++ src/main/java/com/zm/tbshop/TbshopApplication.java
@@ -0,0 +1,13 @@
+package com.zm.tbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zm/tbshop/TbshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zm.tbshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
